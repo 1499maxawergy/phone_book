9ca7eb54a58615b9c0686eedc06aae4b482b467c
Maxawergy <maxawergy@yandex.ru>
2022-09-14 20:47:18 +0300

    Changed losing styles for '/api' and '/admin'

--- phone_book/settings.py
+++ phone_book/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lh_u-x93#az#wpp%0mzwv@j$s%xn-$k%9825m*sssm%hnjdil='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
