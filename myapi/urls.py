7a354902a7426935e5954dc1654006ca07b61448
Maxawergy <maxawergy@yandex.ru>
2022-09-25 18:56:08 +0300

    Added Swagger to phone_book project: 	modified:   db.sqlite3 	modified:   myapi/__pycache__/urls.cpython-310.pyc 	modified:   myapi/__pycache__/views.cpython-310.pyc 	modified:   myapi/urls.py 	modified:   phone_book/__pycache__/settings.cpython-310.pyc 	modified:   phone_book/settings.py

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,7 +1,25 @@
 # myapi/urls.py
-from django.urls import include, path
+from urllib import request
+from django.urls import include, path, re_path
 from . import views
+from drf_yasg import openapi  # new
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view  # new
 
+#Swagger
+
+schema_view = get_schema_view(  # new
+    openapi.Info(
+        title="Phonebook API",
+        default_version='v1.1',
+        description="Documentation for work with Phonebook project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="maxawergy@yandex.ru"),
+        license=openapi.License(name="GNU General Public License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -17,5 +35,6 @@ urlpatterns = [
     path('update/<int:pk>',views.updatePT, name='updatePT'),
     path('edit/<int:pk>',views.update, name='edit'),
     path('api', views.PhonebookViews.as_view()),
-    path('api/<int:id>', views.PhonebookViews.as_view())
+    path('api/<int:id>', views.PhonebookIDViews.as_view()),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
